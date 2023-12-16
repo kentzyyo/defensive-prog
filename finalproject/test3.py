bea161dbb62a6dabae50e436d0117a217db33c5b
selwyn <selwyn.onedo14@gmailcom>
2023-12-17 02:17:11 +0800

    mopdify

--- finalproject/test3.py
+++ finalproject/test3.py
@@ -3,12 +3,13 @@ import tkinter as tk
 from geopy.geocoders import Nominatim
 from tkinter import ttk, messagebox
 from timezonefinder import TimezoneFinder
+
 from datetime import datetime
 import requests
 import pytz
 
 root=Tk()
-root.title=("Weather App")
+root.title=("Group 5-Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False, False)
 
